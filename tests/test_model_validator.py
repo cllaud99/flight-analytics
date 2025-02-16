9fd9ba0990f77cc3db6c76bf8dfcb81078a6ec41
Claudio Pontes <claudiohpontes@gmail.com>
2025-02-16 10:23:31 -0300

    Removida importações desnecessárias

--- tests/test_model_validator.py
+++ tests/test_model_validator.py
@@ -1,14 +1,6 @@
 import os
 import sys
-from datetime import datetime
-from io import StringIO
-from typing import Optional
-
-import pandas as pd
 import pytest
-from pydantic import BaseModel, ValidationError, field_validator
-from pydantic.types import StringConstraints, conint
-from typing_extensions import Annotated
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
